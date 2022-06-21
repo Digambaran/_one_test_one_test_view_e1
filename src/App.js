5186c99b8028548fe241c64b938b7bc3d8432ade
Digambaran <arjunsreekumar.1@gmail.com>
2022-06-21 09:58:56 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import one_test_view_e1 from './one_test_view_e1'
+export default function App() {
+  return (
+    <div>
+      <one_test_view_e1/>
+    </div>
+  )
+}
---
+++ src/one_test_view_e1.js
@@ -0,0 +1,8 @@
+import React from 'react'
+// import env from 'env'
+export const one_test_view_e1 = () => {
+  return (
+   <p>Hello from one_test_view_e1 </p>
+  )
+}
+export default one_test_view_e1
